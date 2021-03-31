b490568c60f8a79f2cbf3aeece50ce454d61b44a
hafizadit <mha.hafiz21@gmail.com>
2021-04-01 00:14:07 +0700

    Menambahkan syarat agar program mengulang ketika nilai > 100

--- I0320064_Soal2_Tugas5.py
+++ I0320064_Soal2_Tugas5.py
@@ -7,33 +7,38 @@ print(endCenter)
 print("="*50)
 print("")
 
-# Input
-nama = input("Masukkan nama anda :")
-nama = nama.title()
-nilai = float(input("Masukkan nilai anda :"))
+# Fungsi
+def nilaiKonversi():
+    nama = input("Masukkan nama anda :")
+    nama = nama.title()
+    nilai = float(input("Masukkan nilai anda :"))
+    if nilai > 100:
+        print(""""Maaf, nilai yang anda masukkan salah.
+        Silakan Mengulang!""")
+        nilaiKonversi()
+    elif nilai >= 85:
+        konversi = "A"
+        print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+    elif nilai >= 80:
+        konversi = "A-"
+        ("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+    elif nilai >= 75:
+        konversi = "B+"
+        print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+    elif nilai >= 70:
+        konversi = "B"
+        print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+    elif nilai >= 65:
+        konversi = "C+"
+        print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+    elif nilai >= 60:
+        konversi = "C"
+        print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+    else:
+        konversi = "E"
+        print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
 
-# If
-if nilai >= 85:
-    konversi = "A"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
-elif nilai >= 80:
-    konversi = "A-"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
-elif nilai >= 75:
-    konversi = "B+"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
-elif nilai >= 70:
-    konversi = "B"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
-elif nilai >= 65:
-    konversi = "C+"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
-elif nilai >= 60:
-    konversi = "C"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
-else:
-    konversi = "E"
-    print("\nHalo %s! Nilai anda setelah dikonversi adalah"%nama, konversi)
+nilaiKonversi()
 
 # Footer
 print("")
